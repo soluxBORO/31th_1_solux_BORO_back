066cd563ad78128a949a8cc90e49e574f64cac14
yoojinche <chesunny917@gmail.com>
2026-06-01 02:25:30 +0900

    feat: 초기 세팅

---
+++ src/main/java/com/boro/BoroApplication.java
@@ -0,0 +1,13 @@
+package com.boro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boro/BoroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
